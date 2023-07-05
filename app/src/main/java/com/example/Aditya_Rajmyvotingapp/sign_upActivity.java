80a1552820c7751e8d3d74da9700cbccd5e73db6
Aditya Raj <araj59197@gmail.com>
2023-07-05 20:39:58 +0530

    voting App

--- app/src/main/java/com/example/Aditya_Rajmyvotingapp/sign_upActivity.java
+++ app/src/main/java/com/example/Aditya_Rajmyvotingapp/sign_upActivity.java
@@ -1,22 +1,18 @@
-package com.example.myvotingapp;
+package com.example.Aditya_Rajmyvotingapp;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.Calendar;
 
 public class sign_upActivity extends AppCompatActivity {
 
